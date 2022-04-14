11890230e165c2ff5562c9e4262205959bf25c7c
fxphunguyen <fxphunguyen@gmail.com>
2022-04-14 11:09:41 +0700

    Tinh lai suat ngan hang

--- VongLap/For.java
+++ VongLap/For.java
@@ -2,7 +2,7 @@ package VongLap;
 
 public class For {
     public static void main(String[] args) {
-        for (int i = 1; i <= 5; i++) {
+        for (short i = 1; i <= 2; i+= 0.1) {
             System.out.println(i);
         }
     }
