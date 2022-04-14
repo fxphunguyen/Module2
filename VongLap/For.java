5bab1076b9081285fe5fecd4e930f249342dec1c
fxphunguyen <fxphunguyen@gmail.com>
2022-04-14 14:07:26 +0700

    Tim uoc chung nho nhat cua 2 so

--- VongLap/For.java
+++
@@ -1,9 +0,0 @@
-package VongLap;
-
-public class For {
-    public static void main(String[] args) {
-        for (short i = 1; i <= 2; i+= 0.1) {
-            System.out.println(i);
-        }
-    }
-}
