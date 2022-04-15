59fc11d696d6e345b4a69aa98344a9fb24743947
fxphunguyen <fxphunguyen@gmail.com>
2022-04-15 14:59:17 +0700

    Tim gia tri lon nhat trong mang

---
+++ Mang/Check_Number.java
@@ -0,0 +1,18 @@
+package Mang;
+
+public class Check_Number {
+   public static void main(String[] args) {
+       System.out.println("Gia tri lon nhat la: "+ checkNumber() );
+   }
+
+    public static int checkNumber(){
+        int[] arr = {10, 3, 2, 20, 7, 9, 6};
+        int max = Integer.MIN_VALUE;
+        for (int item : arr) {
+           if (item > max) {
+                max = item;
+            }
+        }
+        return max;
+    }
+}
