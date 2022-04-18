39f270c00405c8dccb8b17633dfacd0a8db513f4
fxphunguyen <fxphunguyen@gmail.com>
2022-04-18 16:59:04 +0700

    Tìm sô lân xuât hien cua ký tư trong chuoi

---
+++ Mang/find_index.java
@@ -0,0 +1,22 @@
+package Mang;
+
+import java.util.Scanner;
+
+public class find_index {
+    public static void main(String[] args) {
+        String chuoi;
+        char kyTu = 'a';
+        int count = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào 1 chuỗi bất kỳ: ");
+        chuoi = scanner.nextLine();
+        // Duyệt từ đầu đến cuối chuỗi
+        for (int i = 0; i < chuoi.length(); i++) {
+            // Nếu ký tự tại vị trí i bằng 'a' thì tăng count lên 1
+            if (chuoi.charAt(i) == kyTu) {
+                count++;
+            }
+        }
+        System.out.println("Số lần xuất hiện của ký tự " + kyTu + "trong chuỗi " + chuoi + " = " + count );
+    }
+}
