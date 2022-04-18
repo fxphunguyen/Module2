db6bf070f6e014934a0ad072998863e8c8b88882
fxphunguyen <fxphunguyen@gmail.com>
2022-04-18 15:17:21 +0700

    Tinh tong tren duong cheo chinh cua ma tran vuong

---
+++ Mang/Sum_Main_Matrix.java
@@ -0,0 +1,37 @@
+package Mang;
+
+import java.util.Scanner;
+
+public class Sum_Main_Matrix {
+    public static void main(String[] args) {
+        int n; // bậc thang của ma trận vuông
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        int Array[][]= new int[n][n];
+        System.out.println("Nhập các phần tử cho ma trận: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println("Array["+ i +"]["+ j +"] =");
+                Array[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("Ma trận Array vừa nhập: ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(Array[i][j] + "\t");
+            }
+            System.out.println("\n");
+        }
+        // Các phần tử trên đường chéo chính là các phần tử có chỉ số dòng bằng chỉ số cột
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                // Kiểm tra nếu i==j thì mới tính tổng
+                if (i == j) {
+                    sum += Array[i][j];
+                }
+            }
+        }
+        System.out.println("Tổng các phần tử trên đường chéo chính là: " + sum);
+    }
+}
