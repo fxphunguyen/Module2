4bfbff0b9061f3cba76c5e895110f56f2bfcc05e
fxphunguyen <fxphunguyen@gmail.com>
2022-04-27 11:04:19 +0700

    Lop Point va lop MovablePoint

---
+++ Ke_Thua/Point_MovablePoint/MovablePointTest.java
@@ -0,0 +1,10 @@
+package Ke_Thua.Point_MovablePoint;
+
+public class MovablePointTest {
+    public static void main(String[] args) {
+        MovablePoint movablePoint = new MovablePoint();
+        movablePoint.setSpeed(1,2);
+        movablePoint.move();
+        System.out.println(movablePoint.toString());
+    }
+}
---
+++ Ke_Thua/Point_MovablePoint/PointTest.java
@@ -0,0 +1,10 @@
+package Ke_Thua.Point_MovablePoint;
+
+public class PointTest {
+    public static void main(String[] args) {
+        Point point = new Point();
+        point.getX();
+        point.getY();
+        System.out.println(point.toString());
+    }
+}
