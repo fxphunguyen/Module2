24719504a37828cd0fc9be448f49d91008c110fd
fxphunguyen <fxphunguyen@gmail.com>
2022-04-27 08:52:37 +0700

    Lop Circle va lop Cylinder

---
+++ Ke_Thua/Circle_Cylinder/CircleTest.java
@@ -0,0 +1,9 @@
+package Ke_Thua.Circle_Cylinder;
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle circle = new Circle(2.0, "green");
+        circle.getArea();
+        System.out.println(circle.getArea());
+    }
+}
