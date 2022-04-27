ac7198b99d4aeb2b200b979924a738fd2f990f6d
fxphunguyen <fxphunguyen@gmail.com>
2022-04-27 10:17:58 +0700

    Lop Point 2D và lop Point 3D

---
+++ Ke_Thua/Point2D_Point3D/Point2DTest.java
@@ -0,0 +1,10 @@
+package Ke_Thua.Point2D_Point3D;
+
+public class Point2DTest {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(2,1);
+        point2D.getXY();
+        System.out.println(point2D.toString());
+
+    }
+}
