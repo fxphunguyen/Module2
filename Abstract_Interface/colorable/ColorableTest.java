67a7b896a7081009f3818c9e529f6b428d629274
fxphunguyen <fxphunguyen@gmail.com>
2022-05-08 13:56:00 +0700

    trien khai lop list don gian

--- Abstract_Interface/colorable/ColorableTest.java
+++ Abstract_Interface/colorable/ColorableTest.java
@@ -2,7 +2,7 @@ package Abstract_Interface.colorable;
 
 public class ColorableTest {
     public static void main(String[] args) {
-        Shape[] shapes = new Shape[2];
+        Shape[] shapes = new Shape[3];
         shapes[0] = new Shape(4.5);
         shapes[1] = new Shape(5.5);
         shapes[2] = new Shape(2.2);
---
+++ danh_sach/Demo.java
@@ -0,0 +1,20 @@
+package danh_sach;
+
+import java.util.LinkedList;
+
+public class Demo {
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        linkedList.add(1);
+        linkedList.add(3);
+        linkedList.add(2);
+        linkedList.add(4);
+        linkedList.addFirst(5);
+        linkedList.add(1,6);
+        System.out.println("Các phần tử có trong linkedList là: ");
+        for (int node : linkedList) {
+            System.out.println(node + "\t");
+        }
+    }
+}
