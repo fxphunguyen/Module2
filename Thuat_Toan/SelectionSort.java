c21e2eeea5edd15a74450ebe8c4d2546bf80e28e
fxphunguyen <fxphunguyen@gmail.com>
2022-06-08 11:09:15 +0700

    update

---
+++ Thuat_Toan/SelectionSort.java
@@ -0,0 +1,27 @@
+package Thuat_Toan;
+
+public class SelectionSort {
+    static int[] list = {1, 5, 9, 3, 4, 3};
+
+    public static void selectionSort(int[] list) {
+        for (int i = 0; i < list.length - 1; i++) {
+            int currentMin = list[i];
+            int currentMinIndex = i;
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin > list[j]) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        selectionSort(list);
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+    }
+}
